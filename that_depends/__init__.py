6b157934e102274ef5f68b9e7a9412ed2aa64f43
venkatkrishna <venkatkrishna@moe>
2025-02-11 10:44:52 +0100

    Reset to ad3a43fc448c1a21041ea5a8aa6f467f5e9866b8 and made changes for LLM Round 1

--- that_depends/__init__.py
+++ that_depends/__init__.py
@@ -1,8 +1,7 @@
 from that_depends import providers
 from that_depends.container import BaseContainer
 from that_depends.injection import Provide, inject
-from that_depends.providers import container_context, sync_container_context
-from that_depends.providers.context_resources import fetch_context_item
+from that_depends.providers import container_context, fetch_context_item, sync_container_context
 
 __all__ = [
     "container_context",
