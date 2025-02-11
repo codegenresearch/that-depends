3d1d42afa303f5e0aed543b3161bfd3564b4d4a4
venkatkrishna <venkatkrishna@moe>
2025-02-11 18:36:19 +0100

    Reset to 78a6202972062258c1bba568782c841f32c56a6c and made changes for LLM Round 1

--- that_depends/providers/resources.py
+++ that_depends/providers/resources.py
@@ -1,8 +1,6 @@
-import typing
 import warnings
 
 from that_depends.providers.base import AbstractResource, ResourceContext
-
 
 T_co = typing.TypeVar("T_co", covariant=True)
 P = typing.ParamSpec("P")
@@ -18,14 +16,9 @@ class Resource(AbstractResource[T_co]):
         "_context",
     )
 
-    def __init__(
-        self,
-        creator: typing.Callable[P, typing.Iterator[T_co] | typing.AsyncIterator[T_co]],
-        *args: P.args,
-        **kwargs: P.kwargs,
-    ) -> None:
+    def __init__(self, creator, *args, **kwargs):
         super().__init__(creator, *args, **kwargs)
-        self._context: typing.Final[ResourceContext[T_co]] = ResourceContext(is_async=self._is_async)
+        self._context = ResourceContext(is_async=self._is_async)
 
     def _fetch_context(self) -> ResourceContext[T_co]:
         return self._context
@@ -35,11 +28,6 @@ class Resource(AbstractResource[T_co]):
 
 
 class AsyncResource(Resource[T_co]):
-    def __init__(
-        self,
-        creator: typing.Callable[P, typing.AsyncIterator[T_co]],
-        *args: P.args,
-        **kwargs: P.kwargs,
-    ) -> None:
+    def __init__(self, creator, *args, **kwargs):
         warnings.warn("AsyncResource is deprecated, use Resource instead", RuntimeWarning, stacklevel=1)
         super().__init__(creator, *args, **kwargs)
