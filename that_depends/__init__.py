ab6c1285a32b09c19c7bddf9cb960828860fcca7
venkatkrishna <venkatkrishna@moe>
2025-02-11 20:49:58 +0100

    Reset to ad3a43fc448c1a21041ea5a8aa6f467f5e9866b8 and made changes for LLM Round 1

--- that_depends/__init__.py
+++ that_depends/__init__.py
@@ -4,9 +4,22 @@ from that_depends import providers
 from that_depends.container import BaseContainer
 from that_depends.injection import Provide, inject
 from that_depends.providers import (
+    AsyncContextResource,
+    AsyncFactory,
+    AsyncResource,
+    AttrGetter,
+    ContextResource,
+    DIContextMiddleware,
+    Dict,
+    Factory,
+    List,
+    Object,
+    Resource,
+    Selector,
+    Singleton,
     container_context,
-    sync_container_context,
     fetch_context_item,
+    sync_container_context,
 )
 
 __all__ = [
